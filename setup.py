5cce38885c9cc5539d926e8e6b1b9a8e52272705
pauljoo <huiyaojiang@gmail.com>
2020-03-15 17:33:56 +0800

    111111

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017-2017 xpanda.info
+# All rights reserved.
+#
+from distutils.core import setup
+
+setup(name='labs-ml',
+      version='1.0',
+      description='labs machine learning',
+      author='paul joo',
+      author_email='jianghuiyao@outlook.com')
